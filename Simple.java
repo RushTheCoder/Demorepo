042a85f62e70ea2349bf571513927cbe384b8bdb
RushTheCoder <rushikesh.aher@thinkitive.com>
2024-10-15 11:34:37 +0530

    added one sout statement

--- Simple.java
+++ Simple.java
@@ -1,5 +1,6 @@
 public class Simple {
     public static void main(String[] args) {
         System.out.println("This Hello world Program");
+        System.out.println("This is Thinkitive");
     }
 }
